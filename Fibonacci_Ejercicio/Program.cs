1d56e4897da4384174929d38e83cc5171616b909
LEVAN <LEVAN@JOTA>
2022-06-11 17:52:52 -0500

    PRUEBA 2

--- Fibonacci_Ejercicio/Program.cs
+++ Fibonacci_Ejercicio/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Fibonacci_Ejercicio
 {
-    internal class Program
+  public  static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            fibonacci(10);
+
         }
+
+        
+
+        public static int fibonacci(int numero) {
+
+            int PrimerNumero = 0, SegunNumero = 1, SigNumero;
+           
+            
+            
+                if (numero == 0)
+                {
+
+                 return 0;
+
+                }
+            else if (numero ==1 || numero == 2)
+                 {
+
+                 return 1;
+
+                    }else
+                        {
+                            for (int i = 0; i <= numero; i++)
+                            {
+
+                                SigNumero = PrimerNumero;
+                                PrimerNumero = SegunNumero;
+                                SegunNumero = SigNumero + PrimerNumero;
+                                
+                            }
+
+                return PrimerNumero;
+
+            }
+            
+        }
+        
     }
 }
---
+++ PruebasTestFibonacci/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PruebasTestFibonacci;
+using Fibonacci_Ejercicio;
+
+namespace PruebasTestFibonacci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           int result = Fibonacci_Ejercicio.Program.fibonacci(10);
+           Assert.AreEqual(55,result);
+        }
+    }
+}
